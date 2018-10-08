46cb72d14a34384203f387cc4fa861ecffd44312
Dhruv Singal <dhruvsingalabc@gmail.com>
2018-10-07 21:19:30 -0700

    started on lab2

---
+++ lab2/lab2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define A_ROWS 100
+#define AB_COMMONS 100
+#define B_COLUMNS 100
+/*
+ * declaring global arrays for A, B, and the result of AXB
+ **/
+
+int main(){
+	return 1;	
+}
