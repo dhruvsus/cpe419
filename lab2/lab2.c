0bac4b5071af9deeb31e8e16a3f839ee6eb720f4
Dhruv Singal <dhruvsingalabc@gmail.com>
2018-10-10 19:09:48 -0700

    how to read floats from file?

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -1,13 +1,31 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthreadh.h>
 
-#define A_ROWS 100
-#define AB_COMMONS 100
-#define B_COLUMNS 100
-/*
- * declaring global arrays for A, B, and the result of AXB
- **/
-
-int main(){
-	return 1;	
+/* dtime -
+utility routine to return 
+the current wall clock time
+*/
+double dtime()
+{
+    double tseconds = 0.0;
+    struct timeval mytime;
+    gettimeofday(&mytime,(struct timezone*)0);
+    tseconds = (double)(mytime.tv_sec + mytime.tv_usec*1.0e-6);
+    return( tseconds );
 }
+
+//declare constants
+
+#define A_HEIGHT 100
+#define B_WIDTH 200
+#define AB_SHARED 50
+
+//declare global variables
+int A[A_HEIGHT][AB_SHARED];
+int B[AB_SHARED][B_WIDTH];
+int C[A_HEIGHT][B_WIDTH];
+long threads_available;
+
+
