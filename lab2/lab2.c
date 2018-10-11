6a7250461a1795fd587fe63ac2af6c16cbcd1933
Dhruv Singal <dhruvsingalabc@gmail.com>
2018-10-11 02:39:46 -0700

    non threaded work done

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -18,9 +18,9 @@ double dtime()
 
 //declare constants
 
-#define A_HEIGHT 10
-#define B_WIDTH 20
-#define AB_SHARED 5
+#define A_HEIGHT 100
+#define B_WIDTH 90
+#define AB_SHARED 80
 
 //declare global variables
 float A[A_HEIGHT][AB_SHARED];
@@ -28,19 +28,29 @@ float B[AB_SHARED][B_WIDTH];
 float C[A_HEIGHT][B_WIDTH];
 long threads_available;
 
+void matrix_mult_nonthreaded(int a_height,int b_width,int ab_shared){
+	int i,j,k;
+	for(i=0;i<a_height;i++){
+		for(j=0;j<b_width;j++){
+			for(k=0;k<ab_shared;k++){
+				C[i][j]+=A[i][k]*B[k][i];
+			}
+		}
+	}
+	return;
+}
 int main(int argc, char *argv[]){
 	FILE *fp;
-	int i, j;
-	float array_read;
+	int i, j, k;
+	double tstart,tstop,ttime;
 	if(argc!=3)
 		return 0;
-	fp = fopen(argv[2], "r"); // assuming format of command ./a.out <#threads> <name of file>
+	fp = fopen(argv[2], "r");
+	// assuming format of command ./a.out <#threads> <name of file>
 	//fill up A
 	for(i=0;i<AB_SHARED;i++){
 		for(j=0;j<A_HEIGHT;j++){
-			fscanf(fp, "%f", &array_read);
-			A[i][j]=array_read;
-			printf("%f\n",array_read);
+			fscanf(fp, "%f", &A[i][j]);
 		}
 	}
 	//fill up B
@@ -48,7 +58,16 @@ int main(int argc, char *argv[]){
 	{
 		for(j=0;j<AB_SHARED;j++){
 			fscanf(fp, "%f", &B[i][j]);
-			printf("%f\n",B[i][j]);;
 		}
-	}	
+	}
+	//multiply non threaded
+	tstart = dtime();
+	//multiply 1000 times
+	for(k=0;k<1000;k++)
+		matrix_mult_nonthreaded(A_HEIGHT,B_WIDTH,AB_SHARED);
+	tstop=dtime();
+	ttime = tstop-tstart;
+	//Print results for non threaded
+	printf("Secs serial = %10.3lf\n",ttime);
+	return 1;
 }
