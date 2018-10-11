edd7b5a7c35a478c67734cf4b2d0dd621e7ad97a
Dhruv Singal <dhruvsingalabc@gmail.com>
2018-10-11 00:31:55 -0700

    spent an hour worrying about the first float being wrong

--- lab2/genFloatFile.py
+++ lab2/genFloatFile.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 import sys
+import random
 file_name="float_data"
 f=open(file_name,mode="w")
 num_floats=sys.argv[1]
-for i in range(num_floats):
-    f.write
+#generation of floats
+random.seed()
+for i in range(int(num_floats)):
+    n=random.uniform(0,100)
+    f.write("{random_float:f}\n".format(random_float=n))
--- lab2/lab2.c
+++ lab2/lab2.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <pthreadh.h>
-
+#include <pthread.h>
+#include <sys/time.h>
 /* dtime -
 utility routine to return 
 the current wall clock time
@@ -18,33 +18,37 @@ double dtime()
 
 //declare constants
 
-#define A_HEIGHT 100
-#define B_WIDTH 200
-#define AB_SHARED 50
+#define A_HEIGHT 10
+#define B_WIDTH 20
+#define AB_SHARED 5
 
 //declare global variables
-int A[A_HEIGHT][AB_SHARED];
-int B[AB_SHARED][B_WIDTH];
-int C[A_HEIGHT][B_WIDTH];
+float A[A_HEIGHT][AB_SHARED];
+float B[AB_SHARED][B_WIDTH];
+float C[A_HEIGHT][B_WIDTH];
 long threads_available;
 
 int main(int argc, char *argv[]){
 	FILE *fp;
 	int i, j;
-	if(argc!=2)
+	float array_read;
+	if(argc!=3)
 		return 0;
 	fp = fopen(argv[2], "r"); // assuming format of command ./a.out <#threads> <name of file>
 	//fill up A
 	for(i=0;i<AB_SHARED;i++){
 		for(j=0;j<A_HEIGHT;j++){
-			fscanf(fp, "%1f", &A[i][j]);
+			fscanf(fp, "%f", &array_read);
+			A[i][j]=array_read;
+			printf("%f\n",array_read);
 		}
 	}
 	//fill up B
 	for(i=0;i<B_WIDTH;i++)
 	{
 		for(j=0;j<AB_SHARED;j++){
-			fscanf(fp, "%1f", &B[i][j]);
+			fscanf(fp, "%f", &B[i][j]);
+			printf("%f\n",B[i][j]);;
 		}
 	}	
 }
