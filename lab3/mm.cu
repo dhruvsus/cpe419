ea2c380eb29261658adbd7d2fcbf632bbea385e3
Dhruv Singal <dhruvsingalabc@gmail.com>
2018-10-18 04:43:48 -0700

    try on server

--- lab3/mm.cu
+++ lab3/mm.cu
@@ -9,24 +9,24 @@
 #define AB_SHARED 1000
 
 //declare global variables
-float* x;
-float* y;
-float* z;
+float* A;
+float* B;
+float* B;
 
 __global__
 void mm()
 {
 	int i,j,k;
-	int num_rows_per_thread=A_HEIGHT/threads_available;
-	int leftover_rows=A_HEIGHT%threads_available;
+	int threadID = blockDim.x*gridDim.x+threadIdx.x;
+	int gridStride = gridDim.x*blockDim.x;
+	
 	int start_row=threadID*num_rows_per_thread;
 	int stop_row=start_row+num_rows_per_thread;
-	if(threadID==threads_available-1)
-		stop_row=stop_row+leftover_rows;
-	for(i=start_row;i<stop_row;i++){
+
+	for(i=threadID ; i<N; i+=gridStride){
 		for(j=0;j<B_WIDTH;j++){
 			for(k=0;k<AB_SHARED;k++){
-				C[i][j]+=A[i][k]*B[k][i];
+				C[i*1000+j]+=A[k*1000+j]*B[i*1000+k];
 			}
 		}
 	}
@@ -36,9 +36,9 @@ int main(void)
 {
 	// Allocate Unified Memory -- accessible from CPU or GPU
 	int N = A_HEIGHT*AB_SHARED;
-	cudaMallocManaged(&x, N*sizeof(float));
-	cudaMallocManaged(&y, N*sizeof(float));
-	cudaMallocManaged(&z, N*sizeof(float));
+	cudaMallocManaged(&A, N*sizeof(float));
+	cudaMallocManaged(&B, N*sizeof(float));
+	cudaMallocManaged(&C, N*sizeof(float));
 	float r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
 
 	// initialize x and y arrays on the host
@@ -47,19 +47,17 @@ int main(void)
 		y[i] = r;
 	}
 
-	// Launch kernel on 1M elements on the GPU
-	int blockSize = 1024;
-	int numBlocks = (N + blockSize - 1) / blockSize;
-
-	mm<<<numBlocks, blockSize>>>();
+	// Launch kernel on 4*256 threads
+	
+	mm<<<4, 1024>>>();
 
 	// Wait for GPU to finish before accessing on host
 	cudaDeviceSynchronize();
 
 	// Free memory
-	cudaFree(x);
-	cudaFree(y);
-	cudaFree(z);
+	cudaFree(A);
+	cudaFree(B);
+	cudaFree(C);
 
 	return 0;
 }
