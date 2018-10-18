26d08c74f9e578ff99786a20156c9a4749587400
Dhruv Singal <dhruvsingalabc@gmail.com>
2018-10-18 04:07:19 -0700

    todo: mm implement

--- lab3/mm.cu
+++ lab3/mm.cu
@@ -1,41 +1,65 @@
 #include <iostream>
 #include <math.h>
-// CUDA kernel to add elements of two arrays
+#include <cstdlib>
+#include <ctime>
+
+// CUDA kernel to multiply elements of two arrays
+#define A_HEIGHT 1000
+#define B_WIDTH 1000
+#define AB_SHARED 1000
+
+//declare global variables
+float* x;
+float* y;
+float* z;
+
 __global__
-void mm(float *x, float *y, int m, int n, int k)
-     
+void mm()
+{
+	int i,j,k;
+	int num_rows_per_thread=A_HEIGHT/threads_available;
+	int leftover_rows=A_HEIGHT%threads_available;
+	int start_row=threadID*num_rows_per_thread;
+	int stop_row=start_row+num_rows_per_thread;
+	if(threadID==threads_available-1)
+		stop_row=stop_row+leftover_rows;
+	for(i=start_row;i<stop_row;i++){
+		for(j=0;j<B_WIDTH;j++){
+			for(k=0;k<AB_SHARED;k++){
+				C[i][j]+=A[i][k]*B[k][i];
+			}
+		}
+	}
+}
+
 int main(void)
 {
-     int N = 1<<20;
-      float *x, *y;
-     
-      // Allocate Unified Memory -- accessible from CPU or GPU
-      cudaMallocManaged(&x, N*sizeof(float));
-      cudaMallocManaged(&y, N*sizeof(float));
-     
-      // initialize x and y arrays on the host
-      for (int i = 0; i < N; i++) {
-        x[i] = 1.0f;
-        y[i] = 2.0f;
-      }
-     
-      // Launch kernel on 1M elements on the GPU
-      int blockSize = 256;
-      int numBlocks = (N + blockSize - 1) / blockSize;
-      add<<<numBlocks, blockSize>>>(N, x, y);
-     
-      // Wait for GPU to finish before accessing on host
-      cudaDeviceSynchronize();
-     
-      // Check for errors (all values should be 3.0f)
-      float maxError = 0.0f;
-      for (int i = 0; i < N; i++)
-        maxError = fmax(maxError, fabs(y[i]-3.0f));
-      std::cout << "Max error: " << maxError << std::endl;
-     
-      // Free memory
-      cudaFree(x);
-      cudaFree(y);
-     
-      return 0;
-    }
+	// Allocate Unified Memory -- accessible from CPU or GPU
+	int N = A_HEIGHT*AB_SHARED;
+	cudaMallocManaged(&x, N*sizeof(float));
+	cudaMallocManaged(&y, N*sizeof(float));
+	cudaMallocManaged(&z, N*sizeof(float));
+	float r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+
+	// initialize x and y arrays on the host
+	for (int i = 0; i < N; i++) {
+		x[i] = r;
+		y[i] = r;
+	}
+
+	// Launch kernel on 1M elements on the GPU
+	int blockSize = 1024;
+	int numBlocks = (N + blockSize - 1) / blockSize;
+
+	mm<<<numBlocks, blockSize>>>();
+
+	// Wait for GPU to finish before accessing on host
+	cudaDeviceSynchronize();
+
+	// Free memory
+	cudaFree(x);
+	cudaFree(y);
+	cudaFree(z);
+
+	return 0;
+}
