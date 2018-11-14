136637ccaf4c68e424796ffd78975d74f49199b1
Dhruv Singal <dhruvsingalabc@gmail.com>
2018-11-13 21:03:34 -0800

    why is it taking so long?

--- lab5/lab5.cu
+++ lab5/lab5.cu
@@ -1,6 +1,9 @@
 //imports
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
+#include <sys/time.h>
+
 //constants
 #define HEIGHT 3000
 #define WIDTH 3000
@@ -9,19 +12,32 @@
 #define THREADS_PER_BLOCK 128
 #define CACHEAMT 0
 
-__global__ void addMat(int * X, int * Y, int * Z, int numElements, int offset) {
-	int myThreadID = blockIdx.x * blockDim.x + threadIdx.x;
-	if (myThreadID > offset && myThreadID < offset + numElements) {
-		// do the add into Z.
-		Z[myThreadID] = X[myThreadID] + Y[myThreadID];
+__global__ void addMat(int *d_X, int *d_Y, int * d_Z, int numElements) {
+	int myThreadID = (blockIdx.x * blockDim.x) + threadIdx.x;
+	int gridStride = blockDim.x*gridDim.x;
+	int i;
+	for (i=0;myThreadID<numElements;i+=gridStride) {
+		// do the add into d_Z.
+		d_Z[myThreadID] = d_X[myThreadID] + d_Y[myThreadID];
 	}
 }
+
+void matrixAddNonThreaded(int *A, int *B, int *D, int nX, int nY){
+	int row, col;
+	for (row=0; row<nY; row++){
+		for(col=0; col<nX; col++) {
+			D[row*nX+col]=A[row*nX+col]+B[row*nX+col];
+		}
+	}
+}
+
 int main() {
-	int i, *X, *Y, *d_X, *d_Y, *Z, offset, streamSize;
+	int i, *X, *Y, *Z, *D, *d_X, *d_Y, *d_Z, offset, streamSize;
 	//clean GPU
 	cudaDeviceReset();
 	//create streams
 	cudaStream_t streams[NUM_STREAMS];
+	
 	for (i = 0; i<NUM_STREAMS; i++){
 		cudaStreamCreate(&streams[i]);
 	}
@@ -29,12 +45,24 @@ int main() {
 	//allocate matrix X and Y on host
 	cudaMallocHost(&X, HEIGHT * WIDTH * sizeof(int));
 	cudaMallocHost(&Y, HEIGHT * WIDTH * sizeof(int));
+	cudaMallocHost(&Z, HEIGHT*WIDTH*sizeof(int));
+	D=(int*)(malloc(HEIGHT*WIDTH*sizeof(int)));
+
+	//initialize X and Y
+	srand(time(NULL));
+	int r=rand()%100;
+	for (int i = 0; i < HEIGHT*WIDTH; i++) {
+		X[i] = r;
+		Y[i] = r;
+		r=rand();
+	}
 
 	//allocate device versions of X and Y
 	cudaMalloc(&d_X, HEIGHT * WIDTH * sizeof(int));
 	cudaMalloc(&d_Y, HEIGHT * WIDTH * sizeof(int));
-	//allocate matrix Z on the device
-	cudaMalloc(&Z, HEIGHT * WIDTH * sizeof(int));
+	
+	//allocate matrix d_Z on the device
+	cudaMalloc(&d_Z, HEIGHT * WIDTH * sizeof(int));
 
 	// for each stream, copy and add
 	// copy first part of X and Y
@@ -49,9 +77,10 @@ int main() {
 	//cudaStreamSynchronize(streams[1]);
 	for(i=0;i<NUM_STREAMS;i++){
 		//copy memory
-		cudaMemcpyAsync(&d_X[offset], &X[offset], streamSize, cudaMemcpyHostToDevice, streams[i]);
-		cudaMemcpyAsync(&d_Y[offset], &Y[offset], streamSize, cudaMemcpyHostToDevice, streams[i]);
-		addMat<<<NUM_BLOCKS, THREADS_PER_BLOCK, CACHEAMT, streams[i]>>>(d_X, d_Y, Z, streamSize, offset);
+		cudaMemcpyAsync(&d_X[offset], &X[offset], streamSize*sizeof(int), cudaMemcpyHostToDevice, streams[i]);
+		cudaMemcpyAsync(&d_Y[offset], &Y[offset], streamSize*sizeof(int), cudaMemcpyHostToDevice, streams[i]);
+		addMat<<<NUM_BLOCKS, THREADS_PER_BLOCK, CACHEAMT, streams[i]>>>(&d_X[offset], &d_Y[offset], &d_Z[offset], streamSize);
+		offset+=streamSize;
 	}
 	
 	//synchronize
@@ -59,10 +88,29 @@ int main() {
 	for (i = 0; i < NUM_STREAMS; i++){
 		cudaStreamDestroy( streams[i]);
 	}
+	
+	//copy d_Z to host
+	cudaMemcpy(&Z, &d_Z, HEIGHT*WIDTH*sizeof(int), cudaMemcpyDeviceToHost);
+	
+	//host only matrix addition
+	matrixAddNonThreaded(X,Y,D,WIDTH,HEIGHT);
+
+	int row, col;
+	float dif=0;
+	for (row=0; row<HEIGHT; row++){
+		for(col=0; col<WIDTH; col++)
+			dif+=abs(Z[row*WIDTH+col]-D[row*WIDTH+col]);
+	}
+	if(dif < 1) printf("SUCCESS\n");
+	else printf("FAIL\n");
+	printf("%f\n",dif);
+
 	cudaFree(d_X);
 	cudaFree(d_Y);
-	cudaFree(Z);
+	cudaFree(d_Z);
 	cudaFreeHost(X);
 	cudaFreeHost(Y);
+	cudaFreeHost(Z);
+	free(D);
 	return 0;
 }
