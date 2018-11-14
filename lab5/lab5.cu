8b132b87a89a7e86e7401dacade231181e9530f3
Dhruv Singal <dhruvsingalabc@gmail.com>
2018-11-13 19:58:01 -0800

    goddamn it. cuda doesn't work on all the systems

--- lab5/lab5.cu
+++ lab5/lab5.cu
@@ -10,56 +10,59 @@
 #define CACHEAMT 0
 
 __global__ void addMat(int * X, int * Y, int * Z, int numElements, int offset) {
-  int myThreadID = blockIdx.x * blockDim.x + threadIdx.x;
-  if (myThreadID > offset && myThreadID < offset + numElements) {
-    // do the add into Z.
-    Z[myThreadID] = X[myThreadID] + Y[myThreadID];
-  }
+	int myThreadID = blockIdx.x * blockDim.x + threadIdx.x;
+	if (myThreadID > offset && myThreadID < offset + numElements) {
+		// do the add into Z.
+		Z[myThreadID] = X[myThreadID] + Y[myThreadID];
+	}
 }
 int main() {
-  int i, * X, * Y, * d_X, * d_Y, * Z, offset, streamSize;
-  //create streams
-  cudaStream_t streams[NUM_STREAMS];
-  for (i = 0; i<NUM_STREAMS; i++){
-    cudaStreamCreate(&streams[i]);
-  }
+	int i, *X, *Y, *d_X, *d_Y, *Z, offset, streamSize;
+	//clean GPU
+	cudaDeviceReset();
+	//create streams
+	cudaStream_t streams[NUM_STREAMS];
+	for (i = 0; i<NUM_STREAMS; i++){
+		cudaStreamCreate(&streams[i]);
+	}
 
-//allocate matrix X and Y on host
-cudaMallocHost((void ** ) & X, HEIGHT * WIDTH * sizeof(int));
-cudaMallocHost((void ** ) & Y, HEIGHT * WIDTH * sizeof(int));
+	//allocate matrix X and Y on host
+	cudaMallocHost(&X, HEIGHT * WIDTH * sizeof(int));
+	cudaMallocHost(&Y, HEIGHT * WIDTH * sizeof(int));
 
-//allocate device versions of X and Y
-cudaMalloc((void ** ) & d_X, HEIGHT * WIDTH * sizeof(int));
-cudaMalloc((void ** ) & d_Y, HEIGHT * WIDTH * sizeof(int));
-//allocate matrix Z on the device
-cudaMalloc( & Z, HEIGHT * WIDTH * sizeof(int));
+	//allocate device versions of X and Y
+	cudaMalloc(&d_X, HEIGHT * WIDTH * sizeof(int));
+	cudaMalloc(&d_Y, HEIGHT * WIDTH * sizeof(int));
+	//allocate matrix Z on the device
+	cudaMalloc(&Z, HEIGHT * WIDTH * sizeof(int));
 
-// for each stream, copy and add
-// copy first part of X and Y
-// this is sequential because on the default stream
-// maybe use 2 streams, async and synchronize the streams
-streamSize = (HEIGHT * WIDTH) / NUM_STREAMS;
-offset = 0;
-// hopefully NUM_STREAMS>=2
-cudaMemcpyAsync( (void *)&d_X[offset], (void *)X[offset], streamSize, cudaMemcpyHostToDevice, streams[0]);
-cudaMemcpyAsync( (void *)&d_Y[offset], (void *)Y[offset], streamSize, cudaMemcpyHostToDevice, streams[1]);
-cudaStreamSynchronize(streams[0]);
-cudaStreamSynchronize(streams[1]);
-i = 0;
-while (i < NUM_STREAMS) {
-  // do the add async
-  addMat <<< NUM_BLOCKS, THREADS_PER_BLOCK, CACHEAMT, streams[i] >>> (d_X, d_Y, Z, streamSize, offset);
-  if (i < NUM_BLOCKS - 1) {
-    offset = (i + 1) * streamSize;
-    // copy next segment async but be careful not to overwrite
-    cudaMemcpyAsync( (void *)&d_X[offset], (void *)X[offset], streamSize, cudaMemcpyHostToDevice, streams[i]);
-    cudaMemcpyAsync( (void *)&d_Y[offset], (void *)Y[offset], streamSize, cudaMemcpyHostToDevice, streams[i]);
-    i++;
-    cudaStreamSynchronize(streams[i]);
-  }
-}
-for (i = 0; i < NUM_STREAMS; i++){
-    cudaStreamDestroy( streams[i]);
-  }
-return 0;
+	// for each stream, copy and add
+	// copy first part of X and Y
+	// this is sequential because on the default stream
+	// maybe use 2 streams, async and synchronize the streams
+	streamSize = (HEIGHT * WIDTH) / NUM_STREAMS;
+	offset = 0;
+	// hopefully NUM_STREAMS>=2
+	//cudaMemcpyAsync(&d_X[offset], &X[offset], streamSize, cudaMemcpyHostToDevice, streams[0]);
+	//cudaMemcpyAsync(&d_Y[offset], &Y[offset], streamSize, cudaMemcpyHostToDevice, streams[1]);
+	//cudaStreamSynchronize(streams[0]);
+	//cudaStreamSynchronize(streams[1]);
+	for(i=0;i<NUM_STREAMS;i++){
+		//copy memory
+		cudaMemcpyAsync(&d_X[offset], &X[offset], streamSize, cudaMemcpyHostToDevice, streams[i]);
+		cudaMemcpyAsync(&d_Y[offset], &Y[offset], streamSize, cudaMemcpyHostToDevice, streams[i]);
+		addMat<<<NUM_BLOCKS, THREADS_PER_BLOCK, CACHEAMT, streams[i]>>>(d_X, d_Y, Z, streamSize, offset);
+	}
+	
+	//synchronize
+	cudaDeviceSynchronize();
+	for (i = 0; i < NUM_STREAMS; i++){
+		cudaStreamDestroy( streams[i]);
+	}
+	cudaFree(d_X);
+	cudaFree(d_Y);
+	cudaFree(Z);
+	cudaFreeHost(X);
+	cudaFreeHost(Y);
+	return 0;
 }
